a9dbfbf25b62c92f45093e3866d642d06aa14119
unknown <ekpenisiraphael@gmail.com>
2022-11-10 21:17:01 +0530

    Catigories check status btn added

---
+++ src/components/AddNewBook.js
@@ -0,0 +1,9 @@
+const AddNewBook = () => (
+  <form>
+    <input type="text" placeholder="Book Title" />
+    <input type="text" placeholder="Book Author" />
+    <button type="submit">ADD BOOK</button>
+  </form>
+);
+  
+export default AddNewBook;
---
+++ src/components/Categories.js
@@ -0,0 +1,5 @@
+const Categories = ()  => (
+  <button type="submit">Check Status</button>
+);
+
+export default Categories;
