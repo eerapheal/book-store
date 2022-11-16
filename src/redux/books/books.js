b896981418d2d27ce56caaac7d82757d0ed2804c
unknown <ekpenisiraphael@gmail.com>
2022-11-16 18:24:53 +0530

    store set with combinereduce

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,8 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK'
+const REMOVE_BOOK = 'REMOVE_BOOK';
 
-const initialstate  = {
-  books: []
+const initialstate = {
+  books: [],
 };
 
 export const addBook = (payload) => ({
@@ -11,22 +11,23 @@ export const addBook = (payload) => ({
 });
 
 export const removeBook = (payload) => {
-  type: REMOVE_BOOK,
+  typy: REMOVE_BOOK,
   payload
 };
 
 const booksReducer = (state = initialstate, action) => {
   switch (action.type) {
     case ADD_BOOK:
-    return [...state, action.payload]
+      return [...state, action.payload];
+    default: return state;
   }
-  const booksReducer = (state = initialstate, action) => {
-    switch (action.type) {
-      case EMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload)
-      default: return state;
-    }
-}
+};
+const removeReducer = (state = initialstate, action) => {
+  switch (action.type) {
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default: return state;
+  }
 };
 
-export default  booksReducer;
+export default booksReducer;
