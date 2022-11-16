d7cda1fc51bf8dc8a13b4a292ec0b3df667ea86a
unknown <ekpenisiraphael@gmail.com>
2022-11-16 16:07:47 +0530

    add book and remove book decleared. reducer function applied

---
+++ src/redux/books/books.js
@@ -0,0 +1,32 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK'
+
+const initialstate  = {
+  books: []
+};
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => {
+  type: REMOVE_BOOK,
+  payload
+};
+
+const booksReducer = (state = initialstate, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+    return [...state, action.payload]
+  }
+  const booksReducer = (state = initialstate, action) => {
+    switch (action.type) {
+      case EMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload)
+      default: return state;
+    }
+}
+};
+
+export default  booksReducer;
