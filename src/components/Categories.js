5398a274e577b2aa968e4c9d88b57c621fb16507
unknown <ekpenisiraphael@gmail.com>
2022-11-18 07:18:52 +0530

    onClick check status applied

---
+++ src/components/Categories.js
@@ -0,0 +1,29 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable */ 
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
+
+function Categories() {
+  const [messagePlaceholder, setMessage] = useState('');
+  const dispatch = useDispatch();
+  dispatch(checkStatus());
+  const message = useSelector((state) => state.handleStatus);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(message);
+  };
+  return (
+    <>
+      <button
+        onClick={handleSubmit}
+        type="submit"
+      >
+        Check Status
+      </button>
+      <p>{ messagePlaceholder }</p>
+    </>
+  );
+}
+
+export default Categories;
