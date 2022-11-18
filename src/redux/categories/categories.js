1190b96e81b8adb60c07a381c907b40037473d59
unknown <ekpenisiraphael@gmail.com>
2022-11-18 07:23:14 +0530

    state set to empty array with return message

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
+
+const checkStatus = () => ({ type: CHECK_STATUS });
+
+const handleStatus = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+export default handleStatus;
+export { checkStatus };
