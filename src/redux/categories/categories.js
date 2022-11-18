536df3d9e96e43a438fb2319f1bb04baccd014f3
unknown <ekpenisiraphael@gmail.com>
2022-11-18 07:25:12 +0530

    reducer combined

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -6,7 +6,7 @@ const checkStatus = () => ({ type: CHECK_STATUS });
 const handleStatus = (state = [], action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return 'Under construction';
+      return 'Under constructions';
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,15 @@
+/* eslint-disable linebreak-style */
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import handleBook from './books/books';
+import handleStatus from './categories/categories';
 
+const reducers = combineReducers({
+  handleBook,
+  handleStatus,
+});
+const store = configureStore({
+  reducer: reducers,
+});
+
+export default store;
