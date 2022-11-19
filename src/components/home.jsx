e927cf2133c2adfa28721dc6623eba7000989fe3
unknown <ekpenisiraphael@gmail.com>
2022-11-19 12:38:04 +0530

    api post, get, json applied

---
+++ src/components/home.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable linebreak-style */
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/book/book';
+
+const Book = ({ title, author, id }) => {
+  const dispatch = useDispatch();
+
+  const deleted = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <section className="sec">
+      <p>{title}</p>
+      <p>{author}</p>
+      <button type="button" onClick={deleted}>Remove</button>
+    </section>
+  );
+};
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default Book;
